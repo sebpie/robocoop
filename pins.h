f5eebb9c85cbce528737875ce1c59e380c406093
Seb <sebastien.pierrel@gmail.com>
2018-05-14 17:33:14 +0200

    pin assignment

---
+++ pins.h
@@ -0,0 +1,12 @@
+#ifndef ROBOCOOP_PINS
+#define ROBOCOOP_PINS
+
+#define PIN_VERIN_DIR1  3
+#define PIN_VERIN_DIR2  5
+#define PIN_VERIN_EN    6
+
+#define PIN_BT_RX      10
+#define PIN_BT_TX      11
+
+
+#endif
